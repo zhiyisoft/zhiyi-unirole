80b3d196cdd4a80840b7dfdb5a794ebb180222e3
SuYu <suyu@zhiyisoft.com>
2012-11-24 07:14:39 +0800

    去掉test，准备改用spec

--- test/role_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RoleTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Role
-  end
-end
--- test/unit/role/role_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Role
-  class RoleTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
