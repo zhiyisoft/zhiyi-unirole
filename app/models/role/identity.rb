bd821b7268ecf2f37f68e7bb74d8575e1f374b5d
XieGang <xiegang@zhiyisoft.com>
2012-11-23 15:00:33 +0800

    utf-8

--- app/models/role/identity.rb
+++ app/models/role/identity.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module Role
   class Identity < ActiveRecord::Base
     # attr_accessible :title, :body
--- app/models/role/role.rb
+++ app/models/role/role.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module Role
   class Role < ActiveRecord::Base
     # attr_accessible :title, :body
