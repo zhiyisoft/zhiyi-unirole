8bada2fcffecde5aef8dbee3a31133981e58ce7b
XieGang <xiegang@zhiyisoft.com>
2012-11-30 17:30:20 +0800

    加入模拟用户

---
+++ app/controllers/unirole/user_controller.rb
@@ -0,0 +1,16 @@
+#require_bdependency "unirole/unirole_controller"
+
+module Unirole
+  class UserController < UniroleController
+    def index      
+      @users = [
+                {:username =>"test1"},
+                {:username =>"test2"},
+                {:username =>"test3"},
+                {:username =>"test4"},
+                {:username =>"test5"},
+                {:username =>"test6"}
+              ]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Unirole::Engine.routes.draw do 
 
   root :to => "dashboard#index"
-  resources :organs,:only => [:index,:create,:destroy,:update] do
+  resources :organs, :only => [:index,:create,:destroy,:update] do
     collection do
       get "get_parent"
     end
   end
-  resources :rank,:only=>[:index,:create,:destroy,:update]
-  resources :membership,:only=>[:index,:create,:destroy,:update]
-  
+  resources :rank, :only=> [:index,:create,:destroy,:update]
+  resources :membership, :only=> [:index,:create,:destroy,:update]
+  resources :user, :only=> [:index]
 end
