346e119ff2853095d5142f2134ed621e790a7c61
XieGang <xiegang@zhiyisoft.com>
2012-12-01 23:55:59 +0800

    角色配置 for 身份配置

--- app/assets/javascripts/unirole/actor.js
+++ app/assets/javascripts/unirole/actor.js
@@ -1,9 +1,26 @@
-function get_membership(){
-    return $.getJSON("membership.json")
+function save_actor_for_membership(obj){
+    organ_id = obj.parent("span").attr("id").split("_edit")[0]
+    obj.parent("span").children("input[type=checkbox]:checked").each(function(){
+	$.post("actor.json",{membership_id: $(this).val(),organ_id: organ_id},function(result){
+
+	})
+    })
+
+
 }
-function edit_actor_for_membership(){
-    membership = get_membership();
-    for (var i=0;i++;i<membership.length ){    
-	alert(membership[i])
-    }
+
+function edit_actor_for_membership(organ_id){
+    $.getJSON("membership.json",function(result){
+    var html=""
+      if (result.length>0){	   
+	  for( i in result){
+	      html +="<input type='checkbox' name='membership' value='"+result[i]._id+"' />"+result[i].name;
+	  }
+	      html +="<input type='button' value='保存' onclick='save_actor_for_membership($(this))'/>"
+	  
+      }else{
+	  html="获取身份列表失败，请重试！"
+      }
+      $("#"+organ_id+"_edit_actor_for_membership").html(html)
+  })
 }
--- app/controllers/unirole/actor_controller.rb
+++ app/controllers/unirole/actor_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #require_dependency "unirole/unirole_controller"
 
 module Unirole
@@ -7,5 +8,23 @@ module Unirole
       @organs = Organ.where(:parent_id=>nil)
     end
 
+    def create
+      @actor=Actor.new
+      @actor[:membership_id] = params[:membership_id]
+      @actor[:organ_id] = params[:organ_id]
+      if Actor.where(:membership_id =>@actor[:membership_id], :organ_id=>@actor[:organ_id]).size==0
+        if @actor.save 
+          @data={:status=>"save success!"}
+        else
+          @data={:status=>"save error!"}
+        end
+      else
+        @data={:status=>"数据已经存在，无须重复保存!"}
+      end
+      respond_to do |format|
+        format.html
+        format.json {render :json =>@data}
+      end
+    end
   end
 end
