80a62818228b2741bf5cad7f3d4dd550fa0c17ed
SuYu <suyu@zhiyisoft.com>
2012-12-03 21:57:34 +0800

    修改主控页面

--- app/controllers/unirole/unirole_controller.rb
+++ app/controllers/unirole/unirole_controller.rb
@@ -1,10 +1,6 @@
 # require_dependency "unirole/application_controller"
 
 module Unirole
-  class UniroleController < ActionController::Base
-    layout "unirole"
-    def request_layout
-
-    end
+  class UniroleController < ::ApplicationController
   end
 end
