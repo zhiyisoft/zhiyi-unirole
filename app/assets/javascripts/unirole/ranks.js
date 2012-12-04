4e60e6387e71bdfbe70cbd4f0ac09e6ad7d93d65
XieGang <xiegang@zhiyisoft.com>
2012-12-04 17:29:32 +0800

    图标

--- app/assets/javascripts/unirole/ranks.js
+++ app/assets/javascripts/unirole/ranks.js
@@ -1,7 +1,7 @@
 function ajax_update(obj){
     $.ajax({
 	type: "put",
-	url: "ranks/"+obj.parent("td").parent("tr").attr("id"),
+	url: "rank/"+obj.parent("td").parent("tr").attr("id"),
 	data: {key: obj.attr("name"), value: obj.val()},
 	dataType: "json",
 	beforeSend: AjaxStart(obj),
