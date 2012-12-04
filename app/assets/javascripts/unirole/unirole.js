38778b3123ce5b24aec0a3dad928e7a67b8db72e
XieGang <xiegang@zhiyisoft.com>
2012-12-04 16:56:56 +0800

    unirole 修改

--- app/assets/javascripts/unirole/unirole.js
+++ app/assets/javascripts/unirole/unirole.js
@@ -1,2 +1,2 @@
-//= require zhiyisoft
-//= require tipswindown.js
+ducument.write("<script language='javascript' src='zhiyisoft.js'></script>");
+ducument.write("<script language='javascript' src='tipswindown.js'></script>");
