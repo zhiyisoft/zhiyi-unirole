2e550da42f256685c4e1571dc9df1b6b74ab763f
yangjunfeng <yangjunfeng@zhiyisoft.com>
2013-02-01 16:08:54 +0800

    修正

--- app/models/unirole/navicate.rb
+++ app/models/unirole/navicate.rb
@@ -3,7 +3,7 @@ module Unirole
   class Navicate
   	class << self
   	  def all
-  	  	[{:name => "用户管理",:url => "/unirole/users"},{:name => "身份职责",:url => "/unirole/memberships"},{:name => "用户角色",:url => "/unirole/actors"},
+  	  	[{:name => "用户管理",:url => "/unirole/users"},{:name => "身份职责",:url => "/unirole/memberships"},
   	  		{:name => "组织机构",:url => "/unirole/organs"},{:name => "行政等级",:url => "/unirole/ranks"}]
   	  end
   	end
