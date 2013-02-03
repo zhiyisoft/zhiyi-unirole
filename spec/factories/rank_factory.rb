0b354af6d52b19c82e356b2a39136e65f1d132d5
suyu <suyu@zhiyisoft.com>
2013-02-03 11:21:47 +0800

    rank 测试通过

--- spec/factories/rank_factory.rb
+++ spec/factories/rank_factory.rb
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
 FactoryGirl.define do
-  factory :chu_rank, :class => Unirole::Rank do
-    seq 10
-    name "处级"
-  end       
-  factory :ke_rank, :class => Unirole::Rank do
-    seq 20
-    name "科级"
-  end       
-  factory :gu_rank, :class => Unirole::Rank do
-    seq 30
-    name "股级"
+  factory :rank, class: Unirole::Rank do
+    to_create do |x|
+      x.class.find_or_create_by name: x.name, seq: x.seq
+    end
+
+    factory :chu_rank do
+      seq 10
+      name "处级"
+    end
+
+    factory :ke_rank do
+      seq 20
+      name "科级"
+    end
+
+    factory :gu_rank do
+      seq 30
+      name "股级"
+    end
   end       
 end
