cfdcb137d181b56e33d02cfe00724c83ebb267d1
suyu <suyu@zhiyisoft.com>
2013-02-03 11:16:22 +0800

    默认成员识别成功

--- spec/models/unirole/membership_spec.rb
+++ spec/models/unirole/membership_spec.rb
@@ -9,6 +9,10 @@ module Unirole
       [:leader, :member].each {|x| FactoryGirl.create x}
     end
 
+    it 'I18n的默认名称' do
+      Membership.default_name.should == '成员'
+    end
+
     it '按名称定位角色' do
       Membership.of('领导').name.should == '领导'
     end
