33c66406c27e9b542475afd6489e791de3b9b1bf
suyu <suyu@zhiyisoft.com>
2013-03-02 20:09:20 +0800

    新建用户对话框

--- app/cells/unirole/user_cell.rb
+++ app/cells/unirole/user_cell.rb
@@ -1,11 +1,25 @@
 require "cell/rails/helper_api"
 
+
+module Unirole::UserHelpers
+  def users_path(model)
+    "/unirole/users"
+  end
+end
+
+
 class Unirole::UserCell < Cell::Rails
   include Cell::Rails::HelperAPI
+  self._helpers = Unirole::UserHelpers
 
   def display(args)
     @organ = args[:organ]
     @users = @organ ? @organ.users : []
+    render
+  end
+
+  def new(args)
+    @user = args[:user]
     render
   end
 
