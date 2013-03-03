aaada3b76fcab6c3a6c11b9f676e6d14d877d89c
suyu <suyu@zhiyisoft.com>
2013-03-03 08:37:07 +0800

    删除用户时正确的删除显示

--- app/views/unirole/users/create.js
+++ app/views/unirole/users/create.js
@@ -1,2 +1,2 @@
-$('<%= j render @user %>').appendTo('#users').hide().fadeIn();
+$('<%= j render_cell "unirole/user", :show, user: @user %>').appendTo('#users').hide().fadeIn();
 $("#working_area form")[0].reset();
